ae1614848ccc1627dcdf5699077ada8437bca1d4
Oliver Nilsson <oliver.nilsson.05@gmail.com>
2023-09-29 15:50:56 +0200

    klar

---
+++ 7045 typkonvertering/uppgift_1.py
@@ -0,0 +1,6 @@
+bredd = float(input("Ange triangelns bredd: "))
+höjd = float(input("Ange triangelns höjd: "))
+area = bredd * höjd / 2
+print("bredd:", bredd)
+print("höjd:", höjd)
+print("area:", area)
---
+++ 7045 typkonvertering/uppgift_2.py
@@ -0,0 +1,6 @@
+radie = float(input("Ange cirkels radie: "))
+area = radie * radie * 3.14
+omkrets = 2 * radie * 3.14
+print("radie:", radie)
+print("area:", area)
+print("omkrets:", omkrets)
